63d93310c72a9c382610dbeb0a2c767b8fdb15fa
Goultard59 <goultard59@gmail.com>
2023-02-02 14:54:26 +0100

    Update transcription_factor.bash

--- 3_scenic/cistarget_script/transcription_factor.bash
+++ 3_scenic/cistarget_script/transcription_factor.bash
@@ -4,4 +4,4 @@ module load bioinfo/hmmer-3.2.1
 cd ~/work
 
 #search for motif on ensembl 102 peptide sequence
-/usr/local/bioinfo/src/HMMER/hmmer-3.2.1/bin/hmmersearch --tblout /home/adufour/work/cistargetdb/hmmerout.txt --cpu 8 /home/adufour/work/cistargetdb/global.hmm /home/adufour/work/proteome/Sus_scrofa.Sscrofa11.1.pep.all.fa
+/usr/local/bioinfo/src/HMMER/hmmer-3.2.1/bin/hmmsearch --tblout /home/adufour/work/cistargetdb/transcription_factor/hmmerout_108.txt --cpu 8 /home/adufour/work/cistargetdb/transcription_factor/global.hmm /home/adufour/work/genome_and_annotation/proteome/Sus_scrofa.Sscrofa11.1_108.pep.all.fa
